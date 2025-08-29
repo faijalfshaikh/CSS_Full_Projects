58f873804af9c148eb79ebf805d3f3e314df6262
faijalfshaikh <faijalfshaikh@gmail.com>
2025-08-29 07:04:23 +0530

    Added Quiz App

---
+++ Quiz App/script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "Who is the Prime Minister of India?",
+        answers:[
+            {text:"Indira Gandhi", correct:false},
+            {text:"APJ Kalam",correct:false},
+            {text:"Sonia Gandhi",correct:false},
+            {text:"Narendra Modi",correct:false}
+        ]
+    }
+]
